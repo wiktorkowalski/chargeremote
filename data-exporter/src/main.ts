4171cb45126740dbaa3099a66bd8fdaf3f6ef608
Wiktor Kowalski <kowalskiwiktor98@gmail.com>
2021-11-01 16:55:48 +0100

    feature: add swagger

--- data-exporter/src/main.ts
+++ data-exporter/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Data Exporter Service')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.DATAEXPORTER_PORT || 3020);
 }
 bootstrap();
